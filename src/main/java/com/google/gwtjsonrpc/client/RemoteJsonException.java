d4d782730a78c160f02a4989d6570e77cdfd5929
Gert Scholten <gscholt@gmail.com>
2009-03-07 14:17:31 +0100

    Exposed code and error details from jsonrpc ErrorObject in RemoteJsonException.
    Signed-off-by: Gert Scholten <gscholt@gmail.com>

--- src/main/java/com/google/gwtjsonrpc/client/RemoteJsonException.java
+++ src/main/java/com/google/gwtjsonrpc/client/RemoteJsonException.java
@@ -14,17 +14,62 @@
 
 package com.google.gwtjsonrpc.client;
 
+import com.google.gwt.json.client.JSONNull;
+import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Exception given to {@link AsyncCallback#onFailure(Throwable)}.
  * <p>
  * This exception is used if the remote JSON server has returned a well-formed
- * JSON error response. The message from that response is the message of the
- * exception, and other details are left out.
+ * JSON error response.
  */
 public class RemoteJsonException extends Exception {
-  public RemoteJsonException(final String message) {
+  private int code;
+  private JSONValue error;
+
+  /**
+   * Construct a new exception representing a welformed JSON error response.
+   * 
+   * @param message A String value that provides a short description of the
+   *        error.
+   * @param code A number that indicates the actual error that occurred.
+   * @param error A {@link JSONNull}, {@link JSONNumber}, {@link JSONString} or
+   *        {@link JSONObject} value that carries custom and
+   *        application-specific error information.
+   */
+  public RemoteJsonException(final String message, int code, JSONValue error) {
     super(message);
+    this.code = code;
+    this.error = error;
+  }
+
+  public RemoteJsonException(final String message) {
+    this(message, 999, null);
+  }
+
+  /**
+   * Gets the error code.
+   * <p>
+   * Note that the JSON-RPC 1.1 draf does not define error codes yet.
+   * 
+   * @return A number that indicates the actual error that occurred.
+   */
+  public int getCode() {
+    return code;
+  }
+
+  /**
+   * Gets the extra error information.
+   * 
+   * @return <code>null</code> if no error information was specified by the
+   *         server, or a {@link JSONNull}, {@link JSONNumber},
+   *         {@link JSONString} or {@link JSONObject} value.
+   */
+  public JSONValue getError() {
+    return error;
   }
 }
